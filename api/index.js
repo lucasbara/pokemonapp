88021d20255945985bb12666e76898154c1c8b38
Lucas Barallobre <lucasbarallobre@gmail.com>
2023-01-19 15:31:31 -0300

    Delete comments

--- api/index.js
+++ api/index.js
@@ -1,29 +1,10 @@
-//                       _oo0oo_
-//                      o8888888o
-//                      88" . "88
-//                      (| -_- |)
-//                      0\  =  /0
-//                    ___/`---'\___
-//                  .' \\|     |// '.
-//                 / \\|||  :  |||// \
-//                / _||||| -:- |||||- \
-//               |   | \\\  -  /// |   |
-//               | \_|  ''\---/''  |_/ |
-//               \  .-\__  '-'  ___/-. /
-//             ___'. .'  /--.--\  `. .'___
-//          ."" '<  `.___\_<|>_/___.' >' "".
-//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
-//         \  \ `_.   \_ __\ /__ _/   .-` /  /
-//     =====`-.____`.___ \_____/___.-`___.-'=====
-//                       `=---='
-//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-const { getPokemonTypes } = require("./src/handlers/helper.js");
+const { getPokemonTypes } = require("./src/controllers/controllers");
 
 // Syncing all the models at once.
 async function getTypes() {
-  await getPokemonTypes();
+  return await getPokemonTypes();
 }
 
 conn.sync({ force: true }).then(() => {
