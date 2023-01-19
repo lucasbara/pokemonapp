0a923a3b87569e104d69244dee03293de8b44f15
Lucas Barallobre <lucasbarallobre@gmail.com>
2023-01-19 16:02:30 -0300

    Add port

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const { getPokemonTypes } = require("./src/controllers/controllers");
+const { PORT } = process.env || 3001;
 
 // Syncing all the models at once.
 async function getTypes() {
@@ -8,7 +9,7 @@ async function getTypes() {
 }
 
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
+  server.listen(PORT, () => {
     getTypes();
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
